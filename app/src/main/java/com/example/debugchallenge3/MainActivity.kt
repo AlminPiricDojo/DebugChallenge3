8609e70c743e14ce3da415ff106b0ee7fa99d161
AlminPiricDojo <apiric@codingdojo.com>
2021-09-24 10:41:32 -0400

    Ready with BUGS

--- app/src/main/java/com/example/debugchallenge3/MainActivity.kt
+++ app/src/main/java/com/example/debugchallenge3/MainActivity.kt
@@ -61,7 +61,7 @@ class MainActivity : AppCompatActivity() {
     private fun getDefinition(word: String): String{
         var response = ""
         try {
-            response = URL("https://api.dictionaryapi.dev/api/v2/entries/en/$word").readText(Charsets.UTF_8)
+            response = URL("https://api.dictionaryapi.dev/api/v2/entries/en/house").readText(Charsets.UTF_8)
         }catch (e: Exception){
             println("Error: $e")
             Toast.makeText(this, "Unable to get data", Toast.LENGTH_LONG).show()
@@ -77,7 +77,7 @@ class MainActivity : AppCompatActivity() {
             val main = jsonArray[0]
             val word = JSONObject(main.toString()).getString("word")
             val inside = jsonArray.getJSONObject(0).getJSONArray("meanings")
-                .getJSONObject(0).getJSONArray("definitions").getJSONObject(0)
+                .getJSONObject(0)
             val definition = JSONObject(inside.toString()).getString("definition")
             Log.d("MAIN", "WORD: $word $definition")
             definitions.add(arrayListOf(word, definition))
